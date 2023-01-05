f129545f4c05537ee417f177ad090eaba23200e9
Nieda Yacob <121923190+NiedaJacob@users.noreply.github.com>
2023-01-05 23:44:37 +0800

    Tambah galeri

--- main.js
+++ main.js
@@ -7,13 +7,42 @@ function onLoad() {
 
     function initClass() {
         burgerButton = document.querySelector('.nav-mobile');
-
         bindEvents();
+        initSwiper();
+        console.log(isMobileDevice());
     }
 
     function bindEvents() {
         burgerButton.addEventListener('click', () => {
+            console.log('ss');
             burgerButton.classList.toggle('open');
         });
+    }
+
+    function initSwiper() {
+        const swiper = new Swiper('.swiper', {
+            // Optional parameters
+            effect: 'coverflow',
+            direction: 'horizontal',
+            centeredSlides: true,
+            slidesPerView: isMobileDevice() ? '1' : '2',
+            loop: true,
+            // Navigation arrows
+            navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            },
+            coverflowEffect: {
+                rotate: 50,
+                stretch: 0,
+                depth: 100,
+                modifier: 1,
+                slideShadows: false,
+              },
+          });
+    }
+
+    function isMobileDevice() {
+        return /(iPhone|iPad|iPod|iOS|Android)/i.test(navigator.userAgent);
     }
 }
